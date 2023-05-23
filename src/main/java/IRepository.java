3e3f54fdc62c01a330daf27e99ca44d206070344
iamnotfelix <andrei.iliescu03@gmail.com>
2023-05-23 09:59:00 +0300

    Implemented get answers.

--- src/main/java/IRepository.java
+++ src/main/java/IRepository.java
@@ -1,7 +1,9 @@
 import java.util.List;
 
+import models.Answer;
 import models.Question;
 
 public interface IRepository {
-    List<Question> GetQuestions();
+    List<Question> GetQuestions(int current, int questionsPerPage);
+    List<Answer> GetAnswers(int questionId);
 }
--- src/main/java/Repository.java
+++ src/main/java/Repository.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import models.Answer;
 import models.Question;
 
 // import models.Question;
@@ -18,8 +19,10 @@ public class Repository implements IRepository{
     private String password = "rootroot";
 
     @Override
-    public List<Question> GetQuestions() {
-        String sql = "SELECT * FROM questions";
+    public List<Question> GetQuestions(int current, int questionsPerPage) {
+        int skip = (current - 1) * questionsPerPage;
+        
+        String sql = "SELECT * FROM questions ORDER BY id LIMIT ? OFFSET ?";
         List<Question> questions = new ArrayList<Question>();
         
         try {
@@ -27,12 +30,15 @@ public class Repository implements IRepository{
 
             Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
             PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, questionsPerPage);
+            statement.setInt(2, skip);
             ResultSet resultSet = statement.executeQuery();
 
             while (resultSet.next()) {
                 int id = resultSet.getInt("id");
                 String text = resultSet.getString("text");
-                Question question = new Question(id, text);
+                List<Answer> answers = this.GetAnswers(id);
+                Question question = new Question(id, text, answers);
                 questions.add(question);
             }
         } catch (ClassNotFoundException e) {
@@ -42,6 +48,35 @@ public class Repository implements IRepository{
         }
 
         return questions;
+    }
+
+    @Override
+    public List<Answer> GetAnswers(int questionId) {
+        String sql = "SELECT * FROM answers WHERE questionId = ?";
+        List<Answer> answers = new ArrayList<Answer>();
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, questionId);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String text = resultSet.getString("text");
+                boolean isCorrect = resultSet.getBoolean("isCorrect");
+                Answer answer = new Answer(id, text, isCorrect, questionId);
+                answers.add(answer);
+            }
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Cannot find the driver in the classpath!", e);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return answers;
     }
     
 }
